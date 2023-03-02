a55b4616315fe0ed7c38727731c860495cd02f5e
SYED RIYAZ <syedriyaz@192.168.1.6>
2023-03-02 23:03:09 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,15 @@ import '../styles/App.css';
 
 const App = () => {
 
-  let [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div class="ball">
       <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
       <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
